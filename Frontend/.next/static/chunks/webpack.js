b5e124119583cd2e82c124c619b9b3d523c880d7
hiyadillah <ilham.iyadillah@gmail.com>
2022-11-10 16:53:01 +0700

    update index

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4ad520f054b4d7a"; }
+/******/ 		__webpack_require__.h = function() { return "f7c1bbed2841f1d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
