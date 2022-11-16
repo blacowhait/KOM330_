d75620189658a3d16b9bf3d984f002addfcba065
blacowhait <greysyafiqkusuma@apps.ipb.ac.id>
2022-11-16 11:57:41 +0700

    add email service

--- Backend/constant/enum.js
+++ Backend/constant/enum.js
@@ -1,1 +1,2 @@
 exports.titleLower = ["unk", "pst", "div"]
+exports.recordType = ["+","-"]
--- Backend/controller/auth.js
+++ Backend/controller/auth.js
@@ -4,10 +4,12 @@ const UserController = require('../controller/user');
 // const AdminController = require('../controller/admin');
 
 const jwt = require('../service/jwt');
+const Mail = require('../service/mail');
 const { returnMessage } = require('../constant')
 
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
+var MailChecker = require('mailchecker');
 
 class AuthController {
 	static async userRegistration(req, res) {
@@ -19,24 +21,31 @@ class AuthController {
         }
         try {
         	let { name, email, password } = req.value.body;
-        	/*if (!MailChecker.isValid(email)) {
+        	if (!MailChecker.isValid(email)) {
                 return res.status(400).json({ message: returnMessage.auth.EMAIL_NOT_VALID})
-            }*/
+            }
             UserModel.findOne({ email }, async function (err, user) {
             	// If user already exist
                 if (user) return res.status(409).json({ message: `${returnMessage.auth.REGISTER_FAIL}, ${returnMessage.EMAIL_EXIST}` });
 
                 // If not exist, create account
                 let verifyEmailToken = crypto.randomBytes(16).toString('hex');
-                let userData = await UserController.create(name, email, password);
+                let userData = await UserController.create(name, email, password, verifyEmailToken);
 
                 // if there is error when create new user
                 if (userData.error) {
                     return res.status(400).json({ message: userData.message });
                 }
+                console.log(verifyEmailToken)
+                // verif
+                await Mail.sendVerifyEmail(name, email, verifyEmailToken);
 
                 // remove unnecessary information
                 userData = userData.toObject();
+                delete userData.resetPasswordToken;
+                delete userData.verifyEmailToken;
+                delete userData.changeEmailToken;
+                delete userData.verifyEmailDate;
                 delete userData.password;
                 delete userData.createdAt;
                 delete userData.updatedAt;
@@ -59,14 +68,15 @@ class AuthController {
 	static async userLogin(req, res) {
 		try {
 			let { email, password } = req.value.body;
-			UserModel.findOne({ email }, '_id name email title', async function (err, user) {
+			UserModel.findOne({ email }, '_id name email title isVerifiedEmail', async function (err, user) {
                 // console.log(user)
                 if (!user) {
                     return res.status(404).json({ message: `${returnMessage.auth.LOGIN_FAIL}, ${returnMessage.auth.EMAIL_NOT_FOUND}` });
                 }
-                /*if (!user.isVerifiedEmail) {
+                
+                if (!user.isVerifiedEmail) {
                     return res.status(401).json({ message: `${returnMessage.auth.LOGIN_FAIL}, ${returnMessage.auth.EMAIL_NOT_VERIVIED}` });
-                }*/
+                }
 
                 let isMatch = await UserController.login(email, password);
                 if (isMatch) {
@@ -97,11 +107,45 @@ class AuthController {
             return res.status(500).json({
                 success: false,
                 message: e.message,
-                captain: null,
+                user: null,
                 token: null
             })
         }
 	}
+
+    static async verifyEmail(req, res) {
+        try {
+            if (!req.query.email || !req.query.token) {
+                return res.status(400).json({ message: `${returnMessage.DATA_INCOMPLETE}` });
+            }
+
+            UserModel.findOne({ email: req.query.email },
+                '_id name email title isVerifiedEmail verifyEmailToken verifyEmailDate',
+                function (err, user) {
+                    // make sure account exist
+                    if (!user) {
+                        return res.redirect(process.env.FRONT_END_URL + `/email/failed?&message='${returnMessage.auth.EMAIL_VERIFY_FAIL}, ${returnMessage.auth.EMAIL_NOT_FOUND}'`)
+                    }
+
+                    if (user.verifyEmailToken === req.query.token) {
+                        user.isVerifiedEmail = true;
+                        user.verifyEmailToken = null;
+                        user.verifyEmailDate = Date.now();
+                        user.vrfEmailStat = "ALREADY";
+                        user.save();
+
+                        // redirect to front end "Email Verified Page"
+                        // return res.redirect(process.env.FRONT_END_URL + `/account/email/verification/verified?name=${user.name}&email=${user.email}&message='${returnMessage.auth.EMAIL_VERIFY_SUCCESS}'`)
+                        return res.redirect(process.env.FRONT_END_URL + `/account/email/verification?verified=true&email=${user.email}`)
+                    } else {
+                        // return res.redirect(process.env.FRONT_END_URL + `/account/email/verification/failed?name=${user.name}&email=${user.email}&message='${returnMessage.auth.EMAIL_VERIFY_FAIL}, ${returnMessage.auth.TOKEN_INVALID}'&token=${req.query.token}`)
+                        return res.redirect(process.env.FRONT_END_URL + `/account/email/verification?verified=false&email=${user.email}`)
+                    }
+                })
+        } catch (e) {
+            return res.status(500).json({ message: e.message })
+        }
+    }
 }
 
 module.exports = AuthController;
